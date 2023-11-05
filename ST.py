2fd8c5fa7933dd9ee3e8ac88944e0d9b4e3a86f2
Marius GUITTON-FRANTZ <marius.guitton-frantz@etu.univ-nantes.fr>
2023-11-05 17:19:59 +0100

    fix problems

--- ST.py
+++ ST.py
@@ -1,8 +1,20 @@
-import sympy as sp
+import logical_proposition as lp
+
+class State:
+    state: tuple[str]
+    def __init__(self, state):
+        self.state = state
+
+    def __eq__(self, other):
+        return self.state == other.state
+    
+    def __hash__(self):
+        return hash(self)
+    
 
 
 class TransitionSystem:
-    S: set
+    S: set[State]
     Act: set
     transitions: dict
     I: set
@@ -18,8 +30,7 @@ class TransitionSystem:
 
     def is_satisfied(self, s, proposition):
         # Implement the logic to evaluate the proposition for the state s
-        # You can use SymPy or implement your own logic here.
-        sp.eval_expr(proposition, self.L.get(s))
+        return proposition.evaluate(self.L.get(s))
 
     def Post(self, s):
         # Implement the logic to compute the Post(s, act)
@@ -61,40 +72,51 @@ def visiter(s, ST, Phi, R, U, b):
 
 # Test the algorithm
 # Define the set of states
-S = [
-    ["nc1", "nc2", "y=1"],
-    ["p1", "nc2", "y=1"],
-    ["c1", "nc2", "y=0"],
-    ["nc1", "p2", "y=1"],
-    ["nc1", "c2", "y=0"],
-    ["p1", "p2", "y=1"],
-    ["c1", "p2", "y=0"],
-    ["p1", "c2", "y=0"],
-]
+state0 = ("nc1", "nc2", "y=1")
+state1 = ("p1", "nc2", "y=1")
+state4 = ("c1", "nc2", "y=0")
+state2 = ("nc1", "p2", "y=1")
+state5 = ("nc1", "c2", "y=0")
+state3 = ("p1", "p2", "y=1")
+state6 = ("c1", "p2", "y=0")
+state7 = ("p1", "c2", "y=0")
+
+S = set({state0, state1, state2, state3, state4, state5, state6, state7})
+
 # Define the set of actions
 Act = set(["y <- y+1", "(y > 0): y <- y-1", "null"])
 # Define the transition function
-transitions = {["nc1", "nc2", "y=1"]:{["p1", "nc2", "y=1"],["nc1", "p2", "y=1"]},
-                ["p1", "nc2", "y=1"]:{["p1", "p2", "y=1"],["c1", "nc2", "y=0"]},
-                ["c1", "nc2", "y=0"]:{["c1", "p2", "y=0"],["nc1", "nc2", "y=1"]},
-                ["nc1", "p2", "y=1"]:{["p1", "p2", "y=1"],["nc1", "c2", "y=0"]},
-                ["nc1", "c2", "y=0"]:{["p1", "c2", "y=0"],["nc1", "nc2", "y=1"]},
-                ["p1", "p2", "y=1"]:{["c1", "p2", "y=0"],["p1", "c2", "y=0"] },
-                ["c1", "p2", "y=0"]:{["nc1", "p2", "y=1"]},
-                ["p1", "c2", "y=0"]:{["p1", "nc2", "y=1"]},
-               }
+transitions = {
+    state0: [state1, state2],
+    state1: [state3, state4],
+    state2: [state3, state5],
+    state3: [state6, state7],
+    state4: [state0, state6],
+    state5: [state0, state7],
+    state6: [state2],
+    state7: [state1],
+}
 # Define the set of initial states
-I = set([["nc1", "nc2", "y=1"]])
+I = set(state0)
 # Define the set of propositions
 Prop = set(["c1", "c2"])
 # Define the labeling function
-L = {["nc1", "nc2", "y=1"]:set(["not c1", "not c2"]),}
+L = {
+    state0: {"nc1", "nc2"},
+    state1: {"p1", "nc2"},
+    state2: {"nc1", "p2"},
+    state3: {"c1", "c2"},
+    state4: {"c1", "nc2"},
+    state5: {"nc1", "c2"},
+    state6: {"c1","p2"},
+    state7: {"p1", "c2"},
+}
 
 # Define the transition system
 ST = TransitionSystem(S, Act, transitions, I, Prop, L)
 
 # Define the invariant
-Phi = sp.parse_expr("(not crit1 or not crit2)")
+Phi = lp.Or(lp.Not(lp.Variable("c1")), lp.Not(lp.Variable("c2")))
 
 # Verify the invariant
 print(verify_invariant(ST, Phi))
